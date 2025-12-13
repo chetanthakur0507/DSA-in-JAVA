8f492c660e71b87fa4dae8cc9fc23e58ca8890ec
Chetan thakur <chetanthakur0507@gmail.com>
2025-12-13 20:37:04 +0530

    Updated project

---
+++ Queue/using_collectionframework.java
@@ -0,0 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class using_collectionframework {
+    public static void main(String[] args){
+
+        Queue<Integer> q=new ArrayDeque(); 
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(6);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+            
+        
+    }
+}
